05cce6f4995a050765f4ff4d5c1688c42ac3766e
Mochammad Fadholi <mochammad.fadholi.st@gmail.com>
2020-04-15 17:59:16 +0700

    feat: init configuration

---
+++ app/Http/Controllers/TodosController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+ use Illuminate\Http\Request;
+ use Illuminate\Support\Facades\DB;
+
+
+ class TodosController extends Controller
+ {
+     public function index()
+     {
+         // mengambil data dari table pegawai
+         $todos = DB::table('todo')->get();
+
+         // mengirim data pegawai ke view index
+         return json_encode($todos);
+
+     }
+ }
